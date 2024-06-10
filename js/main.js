ba6b5ce836e3b058c3ee0cb5cfb85e85f60a7915
dacarlson6 <143907525+dacarlson6@users.noreply.github.com>
2024-06-10 17:30:21 -0500

    added to main

--- js/main.js
+++ js/main.js
@@ -71,3 +71,21 @@ function cities(){
 
 //call the initialize function when the window has loaded
 window.onload = initialize();
+
+function debugCallback(response){
+	document.querySelector("#mydiv").insertAdjacentHTML('beforeend', 'GeoJSON data: ' + JSON.stringify(myData))
+};
+
+function debugAjax(){
+	
+	var myData;
+	
+	fetch("data/MegaCities.geojson")
+		.then(function(response){
+			debugCallback(response);
+		})
+
+	document.querySelector("#mydiv").insertAdjacentHTML('beforeend' '<br>GeoJSON data:<br>' + JSON.stringify(myData))
+};
+
+document.querySelector("#mydiv").insertAdjacentHTML('beforeend', 'GeoJSON data: ' + JSON.stringify(myData))
